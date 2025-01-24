21a99d33dfd2bf0d859df16b617c8e814a4f927a
LeatherBag011235 <93046087+LeatherBag011235@users.noreply.github.com>
2025-01-24 18:23:04 +0300

    Documentation for Parser class has been added

--- src/data_collection/parser/test_main.py
+++ src/data_collection/parser/test_main.py
@@ -1,14 +1,17 @@
 import sys 
 sys.path.append(r'C:\Users\310\Desktop\Progects_Py\Risk-and-return-prediction-with-LLM\src')
 
+from pathlib import Path
+
 from data_collection.parser.parser_class import Parser
 
 def test_main():
-    years: list[int] = [2022,2023]
+    years: list[int] = [2012, 2013]
     quartrs: list[int] = [3, 4] 
+    save_dir: Path = Path(r"C:\Users\310\Desktop\Progects_Py\data\Parsim_sec_data\raw_data\2013_reports")
 
-    parser = Parser(years, quartrs)
-    parser.get_company_links_object()
+    parser = Parser(years, quartrs, save_dir)
+    print(len(parser.get_company_links_object()))
 
 if __name__ == "__main__":
     test_main()
