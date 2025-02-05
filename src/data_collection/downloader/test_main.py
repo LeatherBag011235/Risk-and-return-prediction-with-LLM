6abc593f5f1d7f1202f59454281295bb02a0deab
LeatherBag011235 <93046087+LeatherBag011235@users.noreply.github.com>
2025-02-05 16:14:01 +0300

    bag: issues with parsing old version of .xcl file fixed

--- src/data_collection/downloader/test_main.py
+++ src/data_collection/downloader/test_main.py
@@ -3,18 +3,18 @@ sys.path.append(r'C:\Users\310\Desktop\Progects_Py\Risk-and-return-prediction-wi
 
 from pathlib import Path
 
-from data_collection.parser.parser_class import Parser
-from data_collection.downloader.downloader_class import Downloader
+from data_collection.parser.parser_snp import ParserSnP
+from data_collection.downloader.dictionary_downloader import DictionaryDownloader
 
 def test_main():
     years: list[int] = [2013]
     quartrs: list[int] = [3] 
-    raw_files_dir: Path = Path(r"C:\Users\310\Desktop\Progects_Py\data\Parsim_sec_data\raw_data\2013_reports")
+    raw_files_dir: Path = Path(r"C:\Users\310\Desktop\Progects_Py\data\Parsim_sec_data\raw_data\test_dir")
 
-    parser = Parser(years, quartrs, raw_files_dir)
-    company_links: dict[str, list[dict]] = parser.get_company_links_object()
+    parser = ParserSnP(years, quartrs, raw_files_dir)
+    company_links: dict[str, list[dict]] = parser.get_company_links()
     
-    downloader = Downloader(company_links, raw_files_dir)
+    downloader = DictionaryDownloader(company_links, raw_files_dir)
     downloader.download_files()
 
 if __name__ == "__main__":
