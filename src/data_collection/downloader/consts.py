6fba38685d8d96de0ea9cb065dd3946489efd43d
LeatherBag011235 <shibanovmaximvl@gmail.com>
2025-02-20 17:55:44 +0300

    Update consts.py

--- src/data_collection/downloader/consts.py
+++ src/data_collection/downloader/consts.py
@@ -4,4 +4,3 @@ import re
 start_pattern = re.compile(r'</ix:resources>\s*</ix:header>', re.IGNORECASE)
 steart_pattern_reserve = re.compile(r'<html>', re.IGNORECASE)
 end_pattern = re.compile(r'</html>', re.IGNORECASE)
-
